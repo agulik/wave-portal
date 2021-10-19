3ce84c5d51fa3919e97aaab3f6ed9b37aa76283d
Andrew Gulik <andrewgulik@gmail.com>
2021-10-18 21:39:45 -0400

    update smart contract

--- contracts/wavePortal.sol
+++ contracts/wavePortal.sol
@@ -5,7 +5,28 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal {
+
+  uint256 totalWaves;
+  mapping(address => uint) public leaderboard;
+
   constructor() {
     console.log("Yo yo - I am a contract and I am smart");
   }
+
+  function wave() public {
+    totalWaves += 1;
+    console.log("%s has waved!", msg.sender);
+    updateLeaderboard(msg.sender);
+  }
+
+  function getTotalWaves() public view returns (uint256) {
+    console.log("we have %d total waves!", totalWaves);
+    return totalWaves;
+  }
+
+  function updateLeaderboard(address _addr) public {
+      leaderboard[_addr] = leaderboard[_addr] + 1;
+      console.log("%s has waved a total of %d times", _addr, leaderboard[_addr]);
+  }
+
 }
